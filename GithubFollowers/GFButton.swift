7c35c54297d5647aab37b7bc3fe0080e6293b9c4
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-01-05 23:56:06 +0600

    Search view controller

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Shadat Vai on 2/1/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GithubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Shadat Vai on 2/1/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
