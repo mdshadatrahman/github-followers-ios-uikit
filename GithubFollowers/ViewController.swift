2a09a150cddb944201803cd69a93573f3a4744af
Shadat Rahman <shadat.rahman.464@gmail.com>
2024-01-05 23:57:27 +0600

    Search view controller

--- GithubFollowers/ViewController.swift
+++ GithubFollowers/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FavoritesListVC.swift
+//  ViewController.swift
 //  GithubFollowers
 //
 //  Created by Shadat Vai on 2/1/24.
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class FavoritesListVC: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemPink
     }
 
+
 }
+
